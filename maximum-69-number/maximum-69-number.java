c1aa4217ef8e5fe513b3bedf72fe3d694f047706
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-04-25 01:50:02 +0200

    Time: 11 ms, Memory: 40.1 MB - LeetHub

--- maximum-69-number/maximum-69-number.java
+++ maximum-69-number/maximum-69-number.java
@@ -3,14 +3,15 @@ class Solution {
         String s = ""+num;
         String ansStr = "";
         for(int i=0;i<s.length();i++){
-            ansStr = "";
             if(s.charAt(i)=='6'){
-                ansStr = s.substring(0,i)+'9'+s.substring(i+1);
+                ansStr += '9'+s.substring(i+1);
                 int ans = Integer.parseInt(ansStr);
                 if(ans>num) {
                     return ans;
                 }
+                break;
             }
+            ansStr+=s.charAt(i);
         }
         return num;
     }
