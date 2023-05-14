a126fbfdc74395c4e533219c5ef44823ff1b0ab7
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-05-15 02:08:07 +0300

    Time: 0 ms, Memory: 42 MB - LeetHub

--- unique-paths-ii/unique-paths-ii.java
+++ unique-paths-ii/unique-paths-ii.java
@@ -1,21 +1,52 @@
 class Solution {
+    //             bottom up approach
+//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            // int m = obstacleGrid.length;
+            // int n = obstacleGrid[0].length;
+            // int dp[][] = new int[m][n];
+            // dp[0][0] = 1;
+            // if(obstacleGrid[0][0]==1) return 0;
+            // for (int row = 0; row < m; row++) {
+            //     for (int col = 0; col < n; col++) {
+            //         if(obstacleGrid[row][col]==1) continue;
+            //         if (row > 0 && obstacleGrid[row-1][col]!=1) {
+            //             dp[row][col] += dp[row - 1][col];
+            //         }
+            //         if (col > 0 && obstacleGrid[row][col-1]!=1) {
+            //             dp[row][col] += dp[row][col - 1];
+            //         }
+            //     }
+            // }
+            // return dp[m - 1][n - 1];
+//     }
+    
+    
+    //-----------------------------
+    // top down approach
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0]==1) return 0;
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int dp[][] = new int[m][n];
-        dp[0][0] = 1;
-        if(obstacleGrid[0][0]==1) return 0;
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if(obstacleGrid[row][col]==1) continue;
-                if (row > 0 && obstacleGrid[row-1][col]!=1) {
-                    dp[row][col] += dp[row - 1][col];
-                }
-                if (col > 0 && obstacleGrid[row][col-1]!=1) {
-                    dp[row][col] += dp[row][col - 1];
-                }
+        int[][] memo = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                memo[i][j]=-1;
             }
         }
-        return dp[m - 1][n - 1];
+        return dp(memo,obstacleGrid,m-1,n-1);
+    }
+    public int dp(int[][] memo, int[][] obstacleGrid, int i, int j){
+        if(i+j==0) return 1;
+        if(obstacleGrid[i][j]==1) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        int ways = 0;
+        if(i>0){
+            ways+= dp(memo,obstacleGrid,i-1,j);
+        }
+        if(j>0){
+            ways+= dp(memo,obstacleGrid,i,j-1);
+        }
+        memo[i][j] = ways;
+        return memo[i][j];
     }
 }
