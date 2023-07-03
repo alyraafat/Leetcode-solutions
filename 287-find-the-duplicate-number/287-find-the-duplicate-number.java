a301167fe4f974390e5618d89bbf8529b5180ef1
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-03 18:17:47 +0300

    Time: 2 ms (98.43%), Space: 57.3 MB (35.35%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int[] temp = new int[nums.length];
+        for(int num:nums){
+            if(temp[num]==1) return num;
+            temp[num]++;
+        }
+        return 0;
+    }
+}
