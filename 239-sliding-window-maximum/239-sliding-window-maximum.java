f1183356e7d27acc9648626c4f34ebe7a95825dc
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-04 01:05:14 +0300

    Time: 211 ms (11.32%), Space: 58.4 MB (85.41%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans = new int[nums.length-k+1];
+        int i=0;int j=0;
+        // int max = Integer.MIN_VALUE;
+        // int max2 = max;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        while(j<nums.length){
+            if((j-i+1)<=k){
+                pq.add(nums[j]);
+                j++;
+            }else if((j-i+1)>k){
+                ans[i] = pq.peek(); 
+                int prev = nums[i];
+                map.put(prev,map.getOrDefault(prev,0)+1);
+                int top = pq.peek();
+                if(pq.peek()==prev) {
+                    pq.poll();
+                    map.put(top,map.getOrDefault(top,0)-1);
+                    if(map.get(top)<=0) map.remove(top);
+                }
+                i++;
+                while(!pq.isEmpty()&&map.containsKey(pq.peek())&&map.get(pq.peek())>0){
+                    top=pq.peek();
+                    pq.poll();
+                    map.put(top,map.getOrDefault(top,0)-1);
+                    if(map.get(top)==0) map.remove(top);
+                }
+            }
+        }
+        ans[i] = pq.peek();
+        return ans;
+    }
+    
+}
