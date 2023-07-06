049d85eda049b15f6941cf53f536ad7ade10b029
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-07 00:40:15 +0300

    Time: 9 ms (97.32%), Space: 46.5 MB (72.02%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -2,58 +2,22 @@ class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         Arrays.sort(products);
         ArrayList<List<String>> ans = new ArrayList<>();
-        ArrayList<String> temp = new ArrayList<>();
-        for(String s: products){
-            temp.add(s);
-        }
-        // System.out.println(temp);
         int left=0;
-        int right=temp.size()-1;
+        int right=products.length-1;
         for(int i=0;i<searchWord.length();i++){
             char curr = searchWord.charAt(i);
-            while(left<=right&&((i>=temp.get(left).length())||(i<temp.get(left).length()&&temp.get(left).charAt(i)<curr)))
+            while(left<=right&&((i>=products[left].length())||(i<products[left].length()&&products[left].charAt(i)<curr)))
                 left++;
-            while(left<=right&&((i>=temp.get(right).length())||(i<temp.get(right).length()&&temp.get(right).charAt(i)>curr))) 
+            while(left<=right&&((i>=products[right].length())||(i<products[right].length()&&products[right].charAt(i)>curr))) 
                 right--;
             int c=left;
             ArrayList<String> a = new ArrayList<>();
             while(c<Math.min(left+3,right+1)){
-                String s1 = temp.get(c);
-            //     // System.out.print(s1+",");
+                String s1 = products[c];
                 if(i<s1.length()&&s1.charAt(i)==curr) a.add(s1);
                 else break;
                 c++;
             }
-            // left=0;
-            // right=temp.size()-1;
-            // System.out.println("left: "+left+",right: "+right); 
-            // while(left<=right){
-            //     int mid=left+(right-left)/2;
-            //     String s = temp.get(mid);
-            //     if(i<s.length()&&s.charAt(i)==curr){
-            //         right=mid-1;
-            //     }else if(i<s.length()&&s.charAt(i)>curr){
-            //         right=mid-1;
-            //     }else{
-            //         left=mid+1;
-            //     }
-            // }
-            // System.out.println("right after:"+right);
-            // int c = right+1;
-            // ArrayList<String> a = new ArrayList<>();
-            // while(c<=right+3&&c<temp.size()){
-            //     String s1 = temp.get(c);
-            //     // System.out.print(s1+",");
-            //     if(i<s1.length()&&s1.charAt(i)==curr) a.add(s1);
-            //     else break;
-            //     c++;
-            // }
-            // System.out.println();
-            // if(a.size()!=0){
-            //     left=right+1;
-            //     right=Math.min(left+2,temp.size()-1);
-            // }
-            // System.out.println(left+", right:"+right+":"+temp);
             ans.add(a);
         }
         return ans;
