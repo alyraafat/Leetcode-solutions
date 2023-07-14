3c496f8935ea86ef64839a831dfa53e0a9e6bd5c
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-14 23:07:45 +0300

    Time: 330 ms (5.07%), Space: 44.9 MB (5.43%) - LeetHub

---
+++ 51-n-queens/51-n-queens.java
@@ -0,0 +1,40 @@
+class Solution {
+    HashSet<Integer> cols = new HashSet<>();
+    HashSet<Integer> diag = new HashSet<>();
+    HashSet<Integer> antidiag = new HashSet<>();
+    ArrayList<List<String>> ans = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++) {
+            sb.append(".");
+        } 
+        backtrack(n, new ArrayList<>(), 0, sb);
+        return ans;
+    }
+    public void backtrack(int n, ArrayList<String> arr, int i,StringBuilder dots){
+        if(arr.size()==n){
+            ans.add(new ArrayList<>(arr));
+            return;
+        }
+        for(int j=i;j<n;j++){
+            StringBuilder sb = new StringBuilder(dots);
+            for(int k=0;k<n;k++){
+                int d = k-j;
+                int ad = k+j;
+                if(!cols.contains(k)&&!diag.contains(d)&&!antidiag.contains(ad)){
+                    cols.add(k);
+                    diag.add(d);
+                    antidiag.add(ad);
+                    sb.replace(k,k+1,"Q");
+                    arr.add(sb.toString());
+                    backtrack(n,arr,j+1,dots);
+                    arr.remove(arr.size()-1);
+                    cols.remove(k);
+                    diag.remove(d);
+                    antidiag.remove(ad);
+                    sb.replace(k,k+1,".");
+                }
+            }
+        }
+    }
+}
