d7ce0e941008f861e54f8ddc0c4452b60cc6fec3
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-18 23:55:33 +0300

    Time: 2 ms (58.51%), Space: 44.4 MB (26.33%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,5 +1,4 @@
 class Solution {
-    HashMap<Integer,ArrayList<Integer>> mapDirected = new HashMap<>();
     HashMap<Integer,ArrayList<Integer>> mapUndirected = new HashMap<>();
     HashSet<Integer> seen = new HashSet<>();
     public boolean validTree(int n, int[][] edges) {
@@ -7,25 +6,13 @@ class Solution {
         if(edges.length!=n-1) return false;
         for(int i=0;i<n;i++){
             mapUndirected.put(i,new ArrayList<>());
-            mapDirected.put(i,new ArrayList<>());
         }
         for(int[] edge:edges){
-            mapDirected.get(edge[0]).add(edge[1]);
             mapUndirected.get(edge[0]).add(edge[1]);
             mapUndirected.get(edge[1]).add(edge[0]);
         }
         dfs(0);
-        if(seen.size()<n) return false;
-        seen=new HashSet<>();
-        // for(int i=0;i<n;i++){
-            // HashSet<Integer> cycle = new HashSet<>();
-            // if(!seen.contains(i)){
-                // seen.add(0);
-                // cycle.add(0);
-                // if(isCycle(0,cycle)) return false;
-            // }
-        // }
-        return true;
+        return seen.size()==n;
     }
     public void dfs(int i){
         for(int node: mapUndirected.get(i)){
@@ -35,16 +22,4 @@ class Solution {
             }
         }
     }
-    // public boolean isCycle(int i,HashSet<Integer> cycle){
-    //     for(int node: mapDirected.get(i)){
-    //         if(!seen.contains(node)){
-    //             cycle.add(node);
-    //             seen.add(node);
-    //             if(isCycle(node,cycle)) return true;
-    //         }else{
-    //             if(cycle.contains(node)) return true;
-    //         }
-    //     }
-    //     return false;
-    // }
 }
