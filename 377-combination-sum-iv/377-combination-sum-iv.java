628fd88d402d619967034d9220544abbe358c7a6
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-23 01:43:00 +0300

    Time: 0 ms (100.00%), Space: 39.9 MB (68.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -6,10 +6,7 @@ class Solution {
         return dp(nums,0,target);
     }
     public int dp(int[] nums,int acc,int target){
-        if(acc==target) {
-            // System.out.println("equal");
-            return 1;
-        }
+        if(acc==target) return 1;
         if(acc>target) return 0;
         if(memo[acc]!=-1){
             return memo[acc];
