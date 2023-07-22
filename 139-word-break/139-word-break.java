500ca2e403c6aae3725b26ffd06bac88bda8bbd0
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-22 17:27:26 +0300

    Time: 6 ms (66.13%), Space: 43.6 MB (40.87%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,27 @@
+class Solution {
+    HashMap<Integer,Integer> memo = new HashMap<>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> wordSet = new HashSet<>();
+        for(String w: wordDict){
+            wordSet.add(w);
+        }
+        if(dp(0,wordSet,new StringBuilder(s))) return true;
+        return false;
+    }
+    public boolean dp(int i, HashSet<String> wordSet,StringBuilder sb){
+        if(i==sb.length()) return true;
+        if(memo.containsKey(i)){
+            return dp(memo.get(i),wordSet,sb);
+        }
+        for(String word: wordSet){
+            if(i+word.length()<=sb.length()){
+                String s = sb.substring(i,word.length()+i);
+                if(s.equals(word)){
+                    memo.put(i,word.length()+i);
+                    if(dp(word.length()+i,wordSet,sb)) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
