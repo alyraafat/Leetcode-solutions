af8a78fd8bdcde9383689a51d46a205807906068
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-23 23:09:51 +0300

    Time: 645 ms (7.46%), Space: 50.6 MB (8.53%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -1,24 +1,19 @@
 class Solution {
     HashMap<Integer,Integer> memo = new HashMap<>();
     public int numSquares(int n) {
-        ArrayList<Integer> nums = new ArrayList<>();
-        int i=1;
-        while(i*i<=n){
-            nums.add(i*i);
-            i++;
-        }
-        // System.out.println(nums);
-        return dp(n,nums);
+        return dp(n);
     }
-    public int dp(int n, ArrayList<Integer> nums){
+    public int dp(int n){
         if(n==0) return 0;
         if(n<0) return Integer.MAX_VALUE;
         if(memo.containsKey(n)) return memo.get(n);
         int res=Integer.MAX_VALUE;
-        for(int num: nums){
-            int x = dp(n-num,nums);
+        int i=1;
+        while(i*i<=n){
+            int x = dp(n-i*i);
             if(x==Integer.MAX_VALUE) continue;
             res=Math.min(res,1+x);
+            i++;
         }
         memo.put(n,res);
         return res;
