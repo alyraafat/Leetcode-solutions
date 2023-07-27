d983f0a7b8243a623fa9a9471f24a925aff800de
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-27 04:36:36 +0300

    Time: 9 ms (69.69%), Space: 44.2 MB (14.44%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,10 +1,13 @@
 class Solution {
-    int[] memo;
+    int[][] memo;
+    int sum;
     public int findTargetSumWays(int[] nums, int target) {
-        // memo=new int[nums.length];
-        // for(int i=0;i<memo.length;i++)
-            // Arrays.fill(memo[i],-1);
-        // Arrays.fill(memo,-1);
+        for(int num: nums){
+            sum+=num;
+        }
+        memo=new int[nums.length][2*sum+1];
+        for(int i=0;i<memo.length;i++)
+            Arrays.fill(memo[i],-1);
         return dp(0,nums,target,0);
     }
     public int dp(int i,int[] nums, int target, int acc){
@@ -12,9 +15,9 @@ class Solution {
             if(acc==target) return 1;
             else return 0;
         }
-        // if(memo[i]!=-1) return memo[i];
+        if(memo[i][sum+acc]!=-1) return memo[i][sum+acc];
         int ways=dp(i+1,nums,target,acc+nums[i])+dp(i+1,nums,target,acc-nums[i]);
-        // memo[i]=ways;
+        memo[i][sum+acc]=ways;
         return ways;
     }
 }
