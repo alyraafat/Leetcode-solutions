a02d536cd27075d01e4d9840dfe246d6a4069f95
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-07-28 14:41:52 +0300

    Time: 87 ms (42.02%), Space: 77.2 MB (23.33%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,15 +1,10 @@
 class Solution {
     String[] strs;
-    // int m;
-    // int n;
     int[][][] memo;
     HashMap<Integer,int[]> map = new HashMap<>();
     public int findMaxForm(String[] strs, int m, int n) {
         this.strs=strs;
-        // this.m=m;
-        // this.n=n;
         memo=new int[strs.length][m+1][n+1];
-        // Arrays.fill(memo,-10);
         for(int i=0;i<memo.length;i++){
             for(int[] arr:memo[i]){
                 Arrays.fill(arr,-10);
@@ -23,7 +18,6 @@ class Solution {
     public int dp(int i,int m, int n){
         if(m<0||n<0) return -1;
         if(i==strs.length) return 0;
-        // if(m==0&&n==0) return 1;
         if(memo[i][m][n]!=-10) return memo[i][m][n];
         int[] curr = map.get(i);
         memo[i][m][n]=Math.max(dp(i+1,m,n),1+dp(i+1,m-curr[0],n-curr[1]));
