e13394fa8bd8c43e407e8197c4e5e522dd03721f
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-08-17 16:31:54 +0300

    Time: 5 ms (36.13%), Space: 41.7 MB (14.26%) - LeetHub

---
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -0,0 +1,71 @@
+class Solution {
+    HashMap<Character, ArrayList<Character>> map;
+    public String alienOrder(String[] words) {
+        map = new HashMap<>();
+        char c = 'a';
+        for(int i=0;i<26;i++){
+            char curr = (char)(c+i);
+            map.put(curr,new ArrayList<>());
+            // map.get(curr).add(curr);
+        }
+        HashSet<Character> found = new HashSet<>();
+        char[] alphabet = new char[26];
+        for(int i=0;i<words.length-1;i++){
+            String curr = words[i];
+            String next = words[i+1];
+            int min = Math.min(curr.length(),next.length());
+            boolean flag = false;
+            if (curr.length() > next.length() && curr.startsWith(next)) {
+                return "";
+            }
+            for(int j=0;j<min;j++){
+                char c1 = curr.charAt(j);
+                char c2 = next.charAt(j);
+                found.add(c1);
+                found.add(c2);
+                if(c1!=c2&&!flag){
+                    map.get(c1).add(c2);
+                    alphabet[c2-'a']++;
+                    flag=true;
+                }
+            }
+            
+            int max = Math.max(curr.length(),next.length());
+            if(curr.length()>next.length()){
+                for(int j=min;j<max;j++) {
+                    found.add(curr.charAt(j));
+                }
+            }else{
+                for(int j=min;j<max;j++) {
+                    found.add(next.charAt(j));
+                }
+            }
+        }
+        if(words.length==1){
+            for(int i=0;i<words[0].length();i++){
+                found.add(words[0].charAt(i));
+            }
+        }
+        StringBuilder top = new StringBuilder();
+        Queue<Character> q = new LinkedList<>();
+        for(int i=0;i<26;i++){
+            char c1 = (char)(c+i);
+            if(alphabet[i]==0&&found.contains(c1)){
+                q.add(c1);
+            }
+        }
+        // System.out.println(q);
+        boolean empty = true;
+        while(!q.isEmpty()){
+            char curr = q.poll();
+            top.append(curr+"");
+            for(char c1: map.get(curr)){
+                empty=false;
+                alphabet[c1-'a']--;
+                if(alphabet[c1-'a']==0) q.add(c1);
+            }
+        }
+        if(top.length()<found.size()) return "";
+        return top.toString();
+    }
+}
