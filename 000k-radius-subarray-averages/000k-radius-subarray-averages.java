0ca4530139b9c16412534e60e434ab389a016047
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-08-18 22:25:29 +0300

    Time: 5 ms, Memory: 61.9 MB - LeetHub

---
+++ 000k-radius-subarray-averages/000k-radius-subarray-averages.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        if(k==0) return nums;
+        int[] avgs = new int[nums.length];
+        // Arrays.fill(avgs,-1);
+        long[] prefixSum = new long[nums.length+1];
+        for(int i=1;i<=nums.length;i++){
+            avgs[i-1]=-1;
+            prefixSum[i]=prefixSum[i-1]+nums[i-1];
+        }
+        for(int i=k;i<nums.length-k;i++){
+            long start = prefixSum[i-k];
+            long end = prefixSum[i+1+k];
+            long avg = (end-start)/(2*k+1);
+            avgs[i] =(int) avg;
+        }
+        return avgs;
+    }
+}
