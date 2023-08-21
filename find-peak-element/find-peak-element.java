44e0e0d4a538003aefd99aa489a8c93a9e148626
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-08-22 02:33:09 +0300

    Time: 0 ms, Memory: 41.3 MB - LeetHub

---
+++ find-peak-element/find-peak-element.java
@@ -0,0 +1,45 @@
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+//         if(nums.length==1) return 0;
+//         return binarySearch(nums);
+//     }
+//     public int binarySearch(int[] nums){
+//         int i=0;
+//         int j=nums.length-1;
+//         while(i<=j){
+//             int mid = (j-i)/2+i;
+//             if(mid==nums.length-1&&nums.length>1&&nums[mid]>=nums[mid-1]) return mid;
+//             if(mid==0&&nums.length>1&&nums[mid]>=nums[mid+1]) return mid;
+//             if(nums[mid]<nums[mid+1]){
+//                 i=mid+1;
+//             }else if(nums[mid]<nums[mid-1]){
+//                 j=mid-1;
+//             }else{
+//                 return mid;
+//             }
+//         }
+//         return i;
+//     }
+// }
+
+class Solution{
+    public int findPeakElement(int[] nums) {
+        return binarySearch(nums);
+    }
+    int binarySearch(int[] nums){
+      if(nums == null || nums.length == 0)
+        return -1;
+      int left = 0, right = nums.length - 1;
+      while(left < right){
+          int mid = left + (right - left) / 2;
+          if(mid!=nums.length-1&&nums[mid] <nums[mid+1]){ 
+              left=mid+1; 
+          }
+          else if(mid!=0&&nums[mid] < nums[mid-1]) { 
+              right = mid; 
+          }else return mid;
+          // else if(mid==0||mid==nums.length-1) return mid;
+      }
+      return right;
+    }
+}
