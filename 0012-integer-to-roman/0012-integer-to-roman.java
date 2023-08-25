b7474406987d826682abf81e0d427b61786ee94e
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-08-25 03:16:04 +0300

    Time: 8 ms (35.24%), Space: 43.9 MB (18.47%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -14,35 +14,35 @@ class Solution {
         map.put(90, "XC");
         map.put(400, "CD");
         map.put(900, "CM");
-        String ans="";
+        StringBuilder ans=new StringBuilder();
         int i = 0;
         int k = num;
         int min = 1;
         int max = 10;
         while(k>0){
-            String f = "";
+            StringBuilder f = new StringBuilder();
             int rem = k%10;
             int n = rem*(int)Math.pow(10,i);
             int mid = max/2;
             if(map.containsKey(n)){
-                f+=map.get(n);
+                f.append(map.get(n));
                 n=0;
             }
             else if(n>=mid&&map.containsKey(mid)){
                 n=n-mid;
-                f+=map.get(mid);
+                f.append(map.get(mid));
             }
             n=n/min;
             while(n>0){
-                f+=map.get(min);
+                f.append(map.get(min));
                 n--;
             }
-            ans = f+ans;
+            ans.insert(0,f.toString());
             k/=10;
             i++;
             min = max;
             max = max*10;
         }
-        return ans;
+        return ans.toString();
     }
 }
