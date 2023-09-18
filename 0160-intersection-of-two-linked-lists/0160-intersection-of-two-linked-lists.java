9f3d802664e7924de63b3d9da129dbd2a46b88ce
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-09-19 02:49:55 +0300

    Time: 1 ms (99.24%), Space: 47.4 MB (8.37%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,19 +11,47 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //Approach 1
+        // ListNode currA = headA;
+        // HashSet<ListNode> set  = new HashSet<>();
+        // while(currA!=null){
+        //     set.add(currA);
+        //     currA = currA.next;
+        // }
+        // ListNode currB = headB;
+        // while(currB!=null){
+        //     if(set.contains(currB)){
+        //         return currB;
+        //     }
+        //     currB = currB.next;
+        // }
+        // return null;
+        //--------------------------
+        //approach 2
         ListNode currA = headA;
-        HashSet<ListNode> set  = new HashSet<>();
+        int a = 0;
         while(currA!=null){
-            set.add(currA);
             currA = currA.next;
+            a++;
         }
         ListNode currB = headB;
+        int b = 0;
         while(currB!=null){
-            if(set.contains(currB)){
-                return currB;
-            }
+            currB = currB.next;
+            b++;
+        }
+        ListNode curr = a>b?headA:headB;
+        for(int i=0;i<Math.abs(a-b);i++){
+            curr=curr.next;
+        }
+        currA = a>b?curr:headA;
+        currB = a>b?headB:curr;
+        while(currA!=null){
+            if(currA==currB) return currA;
+            currA = currA.next;
             currB = currB.next;
         }
         return null;
+        
     }
 }
