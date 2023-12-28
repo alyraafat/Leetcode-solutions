edeccf0d95d91ebb4d439d9668158ea00ed07861
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-12-28 16:23:38 +0200

    Time: 42 ms (56.23%), Space: 56.1 MB (38.85%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -19,15 +19,10 @@ class Solution {
         for (int i = 1; i < n; i++) {
             // Profit if the current job is included.
             int profitIncluding = jobs[i].profit;
-            int lastIndex = -1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (jobs[j].end <= jobs[i].start) {
-                    lastIndex = j;
-                    break;
-                }
-            }
-            if (lastIndex != -1) {
-                profitIncluding += dp[lastIndex];
+            int lastNonConf = findLastNonConflictingJob(jobs,i);
+            
+            if (lastNonConf != -1) {
+                profitIncluding += dp[lastNonConf];
             }
             // Maximum profit up to the i-th job.
             dp[i] = Math.max(profitIncluding, dp[i - 1]);
@@ -35,7 +30,22 @@ class Solution {
         
         return dp[n - 1];
     }
-    
+    private int findLastNonConflictingJob(Job[] jobs, int i) {
+        int low = 0, high = i - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (jobs[mid].end <= jobs[i].start) {
+                if (jobs[mid + 1].end <= jobs[i].start) {
+                    low = mid + 1;
+                } else {
+                    return mid;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1; // No non-conflicting job found
+    }
     class Job {
         int start, end, profit;
         Job(int start, int end, int profit) {
