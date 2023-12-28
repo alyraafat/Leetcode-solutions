b8e76d61c00573243fdf631bca6ae7f61ecc957f
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-12-29 00:39:52 +0200

    Time: 127 ms (13.51%), Space: 57.4 MB (15.83%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,9 +3,22 @@ class Solution {
         int sum = 0;
         for(int num : nums) sum+=num;
         if(sum%2!=0) return false;
-        Boolean[][] memo = new Boolean[nums.length][sum+1];
+        Boolean[][] memo = new Boolean[nums.length][sum/2 +1];
         return topdown(memo,0,sum/2,nums);
     }
+    // public boolean bottomup(int[] nums){
+    //     int sum = 0;
+    //     for(int num : nums) sum+=num;
+    //     if(sum%2!=0) return false;
+    //     Boolean[][] dp = new Boolean[nums.length][sum/2 +1];
+    //     for(int i=0;i<dp.length;i++){
+    //         for(int j=0;j<sum+1;j++){
+    //             if(dp[i][j-nums[i]]){
+    //                 dp[i][j] = true;
+    //             }
+    //         }
+    //     }
+    // }
     public boolean topdown(Boolean[][] memo,int i, int sum, int[] nums){
         if(sum<0) return false;
         if(sum==0) return true;
