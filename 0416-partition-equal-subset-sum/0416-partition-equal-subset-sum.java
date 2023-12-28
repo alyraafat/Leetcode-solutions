0271fbe7d840b03ccf4aaf354deadd14b232baf7
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-12-29 00:58:26 +0200

    Time: 2082 ms (5.03%), Space: 46.4 MB (60.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,41 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        //topdown
+        // int sum = 0;
+        // for(int num : nums) sum+=num;
+        // if(sum%2!=0) return false;
+        // Boolean[][] memo = new Boolean[nums.length][sum/2 +1];
+        // return topdown(memo,0,sum/2,nums);
+        //-----------------------------------------------
+        //bottomup
+        return bottomup(nums);
+    }
+    public boolean bottomup(int[] nums){
         int sum = 0;
         for(int num : nums) sum+=num;
         if(sum%2!=0) return false;
-        Boolean[][] memo = new Boolean[nums.length][sum/2 +1];
-        return topdown(memo,0,sum/2,nums);
+        boolean[][] dp = new boolean[nums.length][sum/2 +1];
+        for(int i=0;i<dp.length;i++){
+            if(nums[i]>sum/2) return false;
+            dp[i][nums[i]] = true;
+        }
+        for(int i=0;i<dp.length;i++){
+            for(int j=nums[i]+1;j<sum/2+1;j++){
+                for(int k=0;k<i;k++){
+                    if(dp[k][j-nums[i]] == true){
+                        dp[i][j] = true;
+                        break;
+                    }
+                }
+                
+            }
+        }
+        boolean res = false; 
+        for(int i=0;i<dp.length;i++){
+            res = res||dp[i][sum/2];
+        }
+        return res;
     }
-    // public boolean bottomup(int[] nums){
-    //     int sum = 0;
-    //     for(int num : nums) sum+=num;
-    //     if(sum%2!=0) return false;
-    //     Boolean[][] dp = new Boolean[nums.length][sum/2 +1];
-    //     for(int i=0;i<dp.length;i++){
-    //         for(int j=0;j<sum+1;j++){
-    //             if(dp[i][j-nums[i]]){
-    //                 dp[i][j] = true;
-    //             }
-    //         }
-    //     }
-    // }
     public boolean topdown(Boolean[][] memo,int i, int sum, int[] nums){
         if(sum<0) return false;
         if(sum==0) return true;
