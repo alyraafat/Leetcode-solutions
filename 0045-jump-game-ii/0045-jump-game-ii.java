c06cc62a5793dfa067b8d588ce98037ef32ab896
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-12-29 21:42:07 +0200

    Time: 555 ms (5.08%), Space: 46.6 MB (6.23%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,7 +1,6 @@
 class Solution {
     public int jump(int[] nums) {
         int[] dp = new int[nums.length];
-        int curr = nums[0];
         for(int i=1;i<dp.length;i++){
             int res = Integer.MAX_VALUE;;
             for(int j=0;j<i;j++){
@@ -13,11 +12,5 @@ class Solution {
         }
         return dp[nums.length-1];
     }
-    // public int topdown(int i, int curr){
-    //     if(i==nums.length) return 0;
-    //     if(memo[i]!=-1) return memo[i];
-    //     if(curr==0){
-    //         memo[i] = topdown(i+1,nums[i]);
-    //     }
-    // }
+
 }
