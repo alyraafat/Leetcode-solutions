c90aa4f519efff0748c93c7e5db6f3e954517e25
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2024-04-17 17:25:10 +0200

    Time: 23 ms, Memory: 55.4 MB - LeetHub

---
+++ contiguous-array/contiguous-array.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int c = 0;
+        int len = 0;
+        map.put(c,-1);
+        for(int i=0;i<nums.length;i++){
+            c += nums[i]==0?1:-1;
+            if(map.containsKey(c)){
+                len = Math.max(len,i-map.get(c));
+            }else{
+                map.put(c,i);
+            }
+        }
+        return len;
+    }
+}
